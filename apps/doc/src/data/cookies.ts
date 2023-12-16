bde15c4030411bdb3950f9348bbb3cfb5de2336f
patiboh <lapatiboh@gmail.com>
2023-12-16 16:54:46 +0100

    🚧 [ui+apps] Manage cookies - cookie constants

---
+++ apps/doc/src/data/cookies.ts
@@ -0,0 +1,17 @@
+const DOC_UI_STATE = 'fat-fuzzy-ui-state'
+const DOC_UI_STYLES = 'fat-fuzzy-ui-styles'
+const DOC_UI_CONTEXT = 'fat-fuzzy-ui-context-reveal'
+const APP_NAV_REVEAL = 'fat-fuzzy-nav-reveal'
+const APP_SIDEBAR_REVEAL = 'fat-fuzzy-sidebar-reveal'
+const APP_SETTINGS_REVEAL = 'fat-fuzzy-settings-reveal'
+const APP_SETTINGS = 'fat-fuzzy-settings-app'
+
+export default {
+	DOC_UI_STATE,
+	DOC_UI_STYLES,
+	DOC_UI_CONTEXT,
+	APP_NAV_REVEAL,
+	APP_SIDEBAR_REVEAL,
+	APP_SETTINGS_REVEAL,
+	APP_SETTINGS,
+}
